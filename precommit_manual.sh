ac8de973df0340ab6748700e36e344c4625c1115
ali bahrampour <78301532+alibahrampour@users.noreply.github.com>
2024-06-27 16:01:54 +0330

    Add files via upload

---
+++ precommit_manual.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ "$RUN_TOOLS" == "true" ]]; then
+    echo "Running black and pyupgrade..."
+    black "$@"
+    pyupgrade "$@"
+else
+    echo "Skipping black and pyupgrade..."
+    exit 0
+fi
